8509dcaf1952ab8d7f6a8c9f7fa771e14f8b1d55
nouftalajmi <alajminouf94@gmail.com>
2023-05-18 17:30:59 +0300

    n

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const employeesJSON = require('./employees.json');
-const managersJSON = require('./managers.json');
+const employeesJSON = require("./employees.json");
+const managersJSON = require("./managers.json");
 
-// Code Here
+class Employees {
+  constructor(name, position, yearJoined, salary) {
+    this.name = name;
+    this.position = position;
+    this.yearJoined = yearJoined;
+    this.salary = salary;
+  }
+  idBadge() {
+    console.log(this.position, this.name);
+  }
+}
+
+class Manager extends Employees {
+  constructor(name, position, yearJoined, salary, bonusPercentage) {
+    this.name = name;
+    this.position = position;
+    this.yearJoined = yearJoined;
+    this.salary = salary;
+    this.bonusPercentage = bonusPercentage;
+    super(Employees);
+  }
+  salaryIncrease(increaseAmount) {}
+}
+bonusPercentage = new Manager(bonusPercentage.Manager());
